20622a19f57124bc980bbec449a84a71c3c6e770
Kirill <salatmail86@gmail.com>
2026-05-31 17:32:11 +0700

    added_logMul

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+//Часть 1
+
 //Перевод строки в вектор
 unsigned char* StrToVec(char* str, size_t* cells)
 {
@@ -144,9 +146,33 @@ bool printVec(unsigned char* vec, size_t len)
     return res;
 }
 
+//Часть 2
+
+//Логическое умножение
+unsigned char* logMul(unsigned char* vec_A, size_t bits_A, unsigned char* vec_B, size_t bits_B)
+{
+    unsigned char *res = NULL;
+
+    if (vec_A && vec_B && bits_A && bits_B && (bits_A == bits_B))
+    {
+        size_t cells = ((bits_A - 1) / 8) + 1;
+        res = (unsigned char*)calloc(cells, sizeof(unsigned char));
+
+        if (res)
+        {
+            for (size_t i = 0; i < cells; i++)
+            {
+                res[i] = vec_A[i] & vec_B[i];
+            }
+        }
+    }
+
+    return res;
+}
+
 int main()
 {
-    //Тесты 1
+//Тесты 1
 
     //11100111 11111111 11100001 01111011
     unsigned char str[32] = "asv00itrgnnfnbfls;e000043994098\0";
@@ -186,11 +212,80 @@ int main()
     free(vec);
     vec = NULL;
 
-    printf("\n");
+    printf("\n\n");
 
-    //Тесты 2
+//Тесты 2
 
+//Умножение
 
+    printf("logMul tests:\n\n");
+
+    size_t cells_1;
+    size_t cells_2;
+
+    unsigned char *vec_1 = StrToVec("0111011\0", &cells_1);
+    unsigned char *vec_2 = StrToVec("01001\0", &cells_2);
+
+    if (vec_1 && vec_2)
+    {
+        unsigned char *vec_res = logMul(vec_1, 7, vec_2, 5);
+
+        printVec(vec_1, 7);
+        printf("\n");
+        printVec(vec_2, 5);
+        printf("\n");
+
+        check = printVec(vec_res, 7);
+        if (!check)
+        {
+            printf("an error occured");
+        }
+
+        free(vec_res);
+        vec_res = NULL;
+    }
+    else
+    {
+        printf("memory was never allocated\n");
+    }
+
+    free(vec_1);
+    vec_1 = NULL;
+    free(vec_2);
+    vec_2 = NULL;
+
+    printf("\n\n");
+
+    vec_1 = StrToVec("110110101110000\0", &cells_1);
+    vec_2 = StrToVec("101110001110110\0", &cells_2);
+
+    if (vec_1 && vec_2)
+    {
+        unsigned char *vec_res = logMul(vec_1, 15, vec_2, 15);
+
+        printVec(vec_1, 15);
+        printf("\n");
+        printVec(vec_2, 15);
+        printf("\n");
+
+        check = printVec(vec_res, 15);
+        if (!check)
+        {
+            printf("an error occured");
+        }
+
+        free(vec_res);
+        vec_res = NULL;
+    }
+    else
+    {
+        printf("memory was never allocated\n");
+    }
+
+    free(vec_1);
+    vec_1 = NULL;
+    free(vec_2);
+    vec_2 = NULL;
 
     return 0;
 }
